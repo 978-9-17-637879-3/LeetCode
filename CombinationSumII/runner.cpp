0591b92c3189af3d3b73870099cf22c6968fda7a
978-9-17-637879-3 <102838865+978-9-17-637879-3@users.noreply.github.com>
2023-08-12 14:45:30 -0400

    [Combination Sum II] only track seen candidates at the same depth level, comment code, change test case

--- CombinationSumII/runner.cpp
+++ CombinationSumII/runner.cpp
@@ -2,44 +2,49 @@
 
 class Solution {
 private:
-    std::vector<std::vector<int>> seenCombinations;
+    std::unordered_map<int, std::vector<int>> depthMap;
     void generateCombinations(const std::vector<int> &candidates, const int &target,
-                              std::vector<std::vector<int>> &combinations, std::vector<int> &path, std::vector<int> &usedIndices,
+                              std::vector<std::vector<int>> &combinations, std::vector<int> &path, std::vector<int> &usedIndices, int depth = 0,
                               const int &sum = 0) {
         for (int i = usedIndices.empty() ? 0 : usedIndices.back() + 1; i < candidates.size(); i++) {
-//            if (std::binary_search(usedIndices.begin(), usedIndices.end(), i)) {
-//                continue;
-//            }
             int newSum = sum + candidates[i];
             if (newSum > target) {
-                return;
+                depthMap.erase(depth);
+                return; // backtrack
             }
 
             // ensure that paths are only generated in ascending order, this prevents duplicates without having to store seen paths, suggested by https://github.com/Ryan4253
             if (!path.empty() && candidates[i] < path.back()) {
-                continue;
+                continue; // goto next node under this parent
             }
 
             path.push_back(candidates[i]);
-            if (std::find(seenCombinations.begin(), seenCombinations.end(),path) != seenCombinations.end()) {
-                path.pop_back();
-                continue;
+            if (depthMap.find(depth) != depthMap.end()) {
+                if (std::binary_search(depthMap[depth].begin(), depthMap[depth].end(), candidates[i])) {
+                    path.pop_back();
+                    continue; // goto next node under this parent
+                }
+                depthMap[depth].push_back(candidates[i]);
+            } else {
+                depthMap[depth] = std::vector<int>{candidates[i]};
             }
-            seenCombinations.push_back(path);
             usedIndices.push_back(i);
 
             if (newSum == target) {
                 combinations.push_back(path);
-                path.pop_back();
-                usedIndices.pop_back();
-                return;
+                path.pop_back(); // reset path for further use
+                usedIndices.pop_back(); // this candidate can still be used on other paths
+                depthMap.erase(depth);
+                return; // backtrack since there can be no other non-duplicate solutions on this path(any other unique solution would be greater than target)
             }
 
-            generateCombinations(candidates, target, combinations, path, usedIndices, newSum);
+            generateCombinations(candidates, target, combinations, path, usedIndices, depth+1, newSum);
 
             path.pop_back();
             usedIndices.pop_back();
         }
+
+        depthMap.erase(depth);
     }
 
 public:
@@ -57,6 +62,6 @@ public:
 };
 
 int main() {
-    std::vector<int> candidates = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
-    (new Solution)->combinationSum2(candidates, 30);
+    std::vector<int> candidates = {3,1,3,5,1,1};
+    (new Solution)->combinationSum2(candidates, 8);
 }
